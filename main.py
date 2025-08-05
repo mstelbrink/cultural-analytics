7ce883395ae6096d6e093a501c274afe173d5e65
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2025-08-05 21:42:42 +0200

    Add scatter plot for song feature

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
+import pandas as pd
+from matplotlib import pyplot as plt
+from utils import chunks
 
 load_dotenv()
 
@@ -16,13 +19,27 @@ offset = 0
 startYear = 1970
 endYear = 2020
 
+df = pd.read_csv("hf://datasets/maharshipandya/spotify-tracks-dataset/dataset.csv")
+
+filtered_df = df[df['track_genre'] == 'pop']
+
 ids = []
-for i in range(startYear, endYear, 10):
-    results = sp.search(q='genre:' + genre + ' year:' + str(i) + '-' + str(i+10), type=type, market=market, limit=limit, offset=offset)
-    for j in range(0, len(results['tracks']['items'])):
-        ids.append(results['tracks']['items'][j]['id'])
+for i in range(0, len(filtered_df.index)):
+    ids.append(filtered_df.iloc[i]['track_id'])
 
 years = {}
-results = sp.tracks(ids)
-for j in range(0, len(ids)):
-    years[results['tracks'][j]['id']] = results['tracks'][j]['album']['release_date']
+
+chunked_ids = list(chunks(ids, 50))
+
+for i in range(0, len(chunked_ids)):
+    results = sp.tracks(chunked_ids[i])
+    for j in range(0, len(chunked_ids[i])):
+        years[results['tracks'][j]['id']] = results['tracks'][j]['album']['release_date']
+
+filtered_df['release_date'] = filtered_df['track_id'].map(years)
+
+sorted_and_filtered_df = filtered_df.sort_values(by="release_date")
+
+plt.scatter(sorted_and_filtered_df['release_date'], sorted_and_filtered_df['danceability'])
+
+plt.show()
