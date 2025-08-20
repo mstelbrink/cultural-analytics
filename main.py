6515a463b9818833def8a7870db6d0db50203435
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2025-08-20 18:07:57 +0200

    Add market restriction

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import os
 import pandas as pd
 from matplotlib import pyplot as plt
 from utils import chunks
+import math
 
 load_dotenv()
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.getenv("SPOTIFY_CLIENT_ID"),
                                                            client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")))
 
+# Parameters
 genre='pop'
 type='track'
 market='US'
@@ -18,47 +20,60 @@ limit = 1
 offset = 0
 startYear = 1970
 endYear = 2020
+rows = 3
+columns = 4
 
 df = pd.read_csv("hf://datasets/maharshipandya/spotify-tracks-dataset/dataset.csv")
 
-filtered_df = df[df['track_genre'] == 'pop']
+df = df[df['track_genre'].str.contains('pop')]
 
-ids = []
-for i in range(0, len(filtered_df.index)):
-    ids.append(filtered_df.iloc[i]['track_id'])
-
-dates = {}
-
+ids = df['track_id']
 chunked_ids = list(chunks(ids, 50))
 
+dates = {}
+published_in_US = {}
 for i in range(0, len(chunked_ids)):
     results = sp.tracks(chunked_ids[i])
     for j in range(0, len(chunked_ids[i])):
         dates[results['tracks'][j]['id']] = results['tracks'][j]['album']['release_date']
+        if market in results['tracks'][j]['album']['available_markets']:
+            published_in_US[results['tracks'][j]['id']] = True
+        else:
+            published_in_US[results['tracks'][j]['id']] = False
 
-filtered_df['release_date'] = filtered_df['track_id'].map(dates)
-filtered_df['release_date'] = pd.to_datetime(filtered_df['release_date'], errors='coerce')
-filtered_df.dropna(subset='release_date', inplace=True)
-filtered_df.set_index('release_date', inplace=True)
+df['release_date'] = df['track_id'].map(dates)
+df['market_US'] = df['track_id'].map(published_in_US)
+
+df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+df.dropna(subset='release_date', inplace=True)
+
+df = df[df['release_date'].dt.year >= startYear]
+df = df[df['release_date'].dt.year < endYear]
+df = df[df['market_US'] == True]
+
+df.set_index('release_date', inplace=True)
 
 features_to_plot = ['acousticness',
                     'danceability',
                     'energy',
                     'instrumentalness',
-                    'key',
                     'liveness',
                     'loudness',
                     'mode',
-                    'speechiness',
                     'tempo',
                     'valence']
 
-yearly_audio_features = filtered_df[features_to_plot].resample('YE').mean()
+yearly_audio_features = df[features_to_plot].resample('YE').mean()
 yearly_audio_features.dropna(inplace=True)
 
-fig, axs = plt.subplots(len(features_to_plot))
+fig, axs = plt.subplots(rows, columns)
+fig.subplots_adjust(hspace=0.33)
 
 for i in range(0, len(features_to_plot)):
-     axs[i].plot(yearly_audio_features.index.year, yearly_audio_features[features_to_plot[i]])
+    x = math.floor(i / columns)
+    y = i % columns
+    axs[x, y].plot(yearly_audio_features.index.year, yearly_audio_features[features_to_plot[i]])
+    axs[x, y].set_title(features_to_plot[i])
+    axs[x, y].grid()
 
 plt.show()
